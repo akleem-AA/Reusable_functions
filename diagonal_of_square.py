2d091666f140ec5a301e7397c6d385385beef736
akleem <74227145+akleem-AA@users.noreply.github.com>
2022-06-26 12:25:32 +0530

    Create diagonal_of_square.py

---
+++ diagonal_of_square.py
@@ -0,0 +1,11 @@
+# ...........Python.......
+#Write a function named diagonal_of_square which takes side length of a square as parameter and returns the diagonal length of that square. in Python
+import math
+
+def diagonal_of_square(side):
+    value = math.sqrt(2)
+    result = value*side
+    return result
+
+print(diagonal_of_square(16))
+#outpur =22.627416997969522

93213b8b56b022b5016c0e3ee499436649a6f28a
akleem <74227145+akleem-AA@users.noreply.github.com>
2022-06-26 12:16:45 +0530

    Create diagonal_of_square.js
    this function takes length of the side of a square as input and return the length of the diagonal of the square

---
+++ diagonal_of_square.js
@@ -0,0 +1,8 @@
+function diagonal_of_square(side){
+    var value = Math.sqrt(2);
+    result = value*side
+    return result ;
+    
+}
+console.log(diagonal_of_square(16))
+//outpur =22.627416997969522
